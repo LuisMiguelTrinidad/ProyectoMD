b34be48691bbe7f41dd8b0cdf518c974f70cd3b4
LuisMiguelTrinidad <lmtrinidadsalvador@gmail.com>
2024-01-18 16:29:04 +0100

    Correciones en los tipos

--- src/Loader.py
+++ src/Loader.py
@@ -3,7 +3,7 @@ import netwulf as nw
 import matplotlib.pyplot as plt
 import Reader
 
-def genera_grafo(diccionario: dict[int, list[tuple[int,float]],tuple[float,float]]) -> nx.Graph:
+def genera_grafo(diccionario: dict[int, tuple[str, list[tuple[int, float]], tuple[float, float]]]) -> nx.Graph:
     diccionarioaux = diccionario.copy()
     diccionarioaux = {clave: diccionarioaux[clave][1] for clave in diccionarioaux}
     grafo = nx.Graph()
@@ -12,14 +12,14 @@ def genera_grafo(diccionario: dict[int, list[tuple[int,float]],tuple[float,float
             grafo.add_edges_from((r, k[0]) for k in diccionarioaux[r])
     return grafo
 
-def test_genera_grafo(diccionario: dict[int, list[tuple[int,float]],tuple[float,float]]) -> None:
+def test_genera_grafo(diccionario: dict[int, tuple[str, list[tuple[int, float]], tuple[float, float]]]) -> None:
     grafoaux = genera_grafo(diccionario)
     diccionarioaux = nx.to_dict_of_lists(grafoaux)
     [print(r[0],":",r[1]) for r in sorted(diccionarioaux.items())[:3]]
     print("...")
     [print(r[0],":",r[1]) for r in sorted(diccionarioaux.items())[-3:]]
 
-def adjudica_pesos(grafo: nx.Graph, diccionario: dict[int, list[tuple[int,float]],tuple[float,float]]) -> nx.Graph:
+def adjudica_pesos(grafo: nx.Graph, diccionario: dict[int, tuple[str, list[tuple[int, float]], tuple[float, float]]]) -> nx.Graph:
     diccionarioaux = diccionario.copy()
     diccionarioaux = {clave: diccionarioaux[clave][1] for clave in diccionarioaux}
     grafoaux = grafo.copy()
@@ -29,14 +29,14 @@ def adjudica_pesos(grafo: nx.Graph, diccionario: dict[int, list[tuple[int,float]
         
     return grafoaux
 
-def test_adjudica_pesos(grafo: nx.Graph, diccionario: dict[int, list[tuple[int,float]],tuple[float,float]]) -> None:
+def test_adjudica_pesos(grafo: nx.Graph, diccionario: dict[int, tuple[str, list[tuple[int, float]], tuple[float, float]]]) -> None:
     grafoaux = adjudica_pesos(grafo, diccionario)
     diccionarioaux = {u: [(v, data['weight']) for v, data in grafoaux[u].items()] for u in grafoaux.nodes()}
     [print(r[0],":",r[1]) for r in sorted(diccionarioaux.items())[:3]]
     print("...")
     [print(r[0],":",r[1]) for r in sorted(diccionarioaux.items())[-3:]]
 
-def adjudica_posiciones(grafo: nx.Graph, diccionario: dict[int, list[tuple[int,float]],tuple[float,float]]) -> nx.Graph:
+def adjudica_posiciones(grafo: nx.Graph, diccionario: dict[int, tuple[str, list[tuple[int, float]], tuple[float, float]]]) -> nx.Graph:
     diccionarioaux = diccionario.copy()
     diccionariox = {clave: diccionarioaux[clave][2][0] for clave in diccionarioaux}
     diccionarioy = {clave: diccionarioaux[clave][2][1] for clave in diccionarioaux}
@@ -45,21 +45,21 @@ def adjudica_posiciones(grafo: nx.Graph, diccionario: dict[int, list[tuple[int,f
     nx.set_node_attributes(grafoaux, diccionarioy, 'y')
     return grafoaux
 
-def test_adjudica_posiciones(grafo: nx.Graph, diccionario: dict[int, list[tuple[int, float]], tuple[float, float]]) -> None:
+def test_adjudica_posiciones(grafo: nx.Graph, diccionario: dict[int, tuple[str, list[tuple[int, float]], tuple[float, float]]]) -> None:
     grafoaux = adjudica_posiciones(grafo, diccionario)
     diccionarioaux = {u: (grafoaux.nodes[u]['x'], grafoaux.nodes[u]['y']) for u in grafoaux.nodes()}
     [print(u, ":", pos) for u, pos in sorted(diccionarioaux.items())[:3]]
     print("...")
     [print(u, ":", pos) for u, pos in sorted(diccionarioaux.items())[-3:]]
 
-def adjudica_etiquetas(grafo: nx.Graph, diccionario: dict[int, list[tuple[int,float]],tuple[float,float]]) -> nx.Graph:
+def adjudica_etiquetas(grafo: nx.Graph, diccionario: dict[int, tuple[str, list[tuple[int, float]], tuple[float, float]]]) -> nx.Graph:
     diccionarioaux = diccionario.copy()
     diccionarioaux = {clave: diccionarioaux[clave][0] for clave in diccionarioaux}
     grafoaux = grafo.copy()
     nx.set_node_attributes(grafoaux, diccionarioaux, 'Label')
     return grafoaux
 
-def test_adjudica_etiquetas(grafo: nx.Graph, diccionario: dict[int, list[tuple[int,float]],tuple[float,float]]) -> None:
+def test_adjudica_etiquetas(grafo: nx.Graph, diccionario: dict[int, tuple[str, list[tuple[int, float]], tuple[float, float]]]) -> None:
     grafoaux = adjudica_etiquetas(grafo, diccionario)
     diccionarioaux = {u: grafoaux.nodes[u]['Label'] for u in grafoaux.nodes()}
     [print(u, ":", label) for u, label in sorted(diccionarioaux.items())[:3]]
