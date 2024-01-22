0ea5f6385458e3e2c8fbf9bc61323eff2437b3c6
LuisMiguelTrinidad <lmtrinidadsalvador@gmail.com>
2024-01-23 00:42:16 +0100

    Commit y a mimir

--- src/Exploring.py
+++ src/Exploring.py
@@ -44,6 +44,30 @@ def rankea(grafo: nx.Graph, dicc_corte: dict, numpart: int):
         ranking[i] = punt
     return ranking
 
+def exploracion(grafo: nx.Graph, puntoinicio: int,participantes: int):
+    grafoaux = grafo.copy()
+    distancias, caminos = nx.single_source_dijkstra(grafoaux, puntoinicio)
+    limite = topedist(grafo, participantes)
+    destinos = puntos_de_corte_y_aislamiento(grafo)
+    for r in distancias:
+        if(distancias[r]>limite):
+            grafoaux.remove_node(r)
+        if (r not in destinos or distancias[r]>limite):
+            caminos.pop(r)
+    
+    return grafoaux, caminos
+
+#Modificamos rankea original, y le damos un punto de origen, y devolvemos el peso que otorga cada nodo de corte a nuestro origen
+def rankea2(grafo: nx.Graph, dicc_corte: dict, numpart: int, origen:int):
+    ranking = dict()
+    toped = topedist(grafo, numpart)
+    destinos = nx.single_source_dijkstra_path_length(grafo,origen)
+    puntuacion_lista = []
+    for i in destinos:
+        if i in dicc_corte.keys() and destinos[i]<toped:
+            puntuacion_lista.append((i,len(dicc_corte[i])*(toped-destinos[i])/toped))
+    return puntuacion_lista
+
 
 
 def main():
@@ -94,6 +118,28 @@ def main():
     print("Imprimiento los diez peores sistemas")
     for r in range(10):
         print(listaordenada[-(r+1)])
+
+    subg, caminos = exploracion(g, 130, 10)
+
+    destinos = puntos_de_corte_y_aislamiento(g).keys()
+
+    print("Hola")
+    [print(r) for r in rankea2(g, a, 10, 130)]
+
+    conjunto = set()
+    for r in caminos:
+        if caminos[r][-1] in destinos:
+            conjunto.update([(caminos[r][k], caminos[r][k+1]) for k in range(len(caminos[r])-1)])
+    nodo_colores = ["#DF954B" if r in a.keys() else "red" if r == 130 else "#9AB5C6" for r in subg.nodes()]
+    arista_colores = ["#DF954B" if ((r[0],r[1]) in conjunto or (r[1],r[0]) in conjunto) else "#9AB5C6" for r in subg.edges()]
+    arista_tamano = [6 if ((r[0],r[1]) in conjunto or (r[1],r[0]) in conjunto) else 3 for r in subg.edges()]
+    nx.draw(subg, with_labels=True, pos=nodo_posiciones, node_color=nodo_colores, edge_color=arista_colores, width=arista_tamano)
+    plt.xlim(-360,360)
+    plt.ylim(-360,360)
+    plt.show()
+
+    print("Hola")
+    [print(r) for r in rankea2(g, a, 10, 130)]
 
 if __name__ == "__main__":
     main()
